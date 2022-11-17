b63fab609f381f28a9da5ccda0d6d9c779f3189f
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-11-17 10:48:19 +0900

    feat/#1 : 공통 컴포넌트 헤더 개발

---
+++ frontend/src/components/Header/index.style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 90%;
+  margin-top: 20px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 5%;
+  img {
+    width: 32px;
+  }
+  button {
+    border: none;
+    background: none;
+  }
+`;
+
+export const RightChildContainer = styled.div`
+  width: 100px;
+  display: flex;
+  justify-content: space-between;
+`;
