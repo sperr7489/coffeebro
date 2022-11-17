5c8816582df92639f2c9910483a4a72d7f0aafbb
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-11-17 10:49:29 +0900

    feat/#1 : 메인페이지 신청자 카드, 모달 개발

---
+++ frontend/src/pages/Main/components/ApplicantCard/index.style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+  display: flex;
+  width: 350px;
+  height: 150px;
+  justify-content: space-around;
+  align-items: center;
+  margin-right: 50px;
+  margin-bottom: 50px;
+  border-radius: 10px;
+  border: 1px solid #c5875b;
+  img {
+    width: 100px;
+    height: 100px;
+  }
+`;
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 200px;
+  height: 100px;
+  cursor: default;
+`;
