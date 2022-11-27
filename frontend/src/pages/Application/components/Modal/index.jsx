38cd0d22dac3a4da86fe0e7128dc80967eb2e350
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-11-27 23:31:26 +0900

    feat/#4 feat : 등록페이지 요청사항 모달 추가 및 애니메이션 추가

--- frontend/src/pages/Application/components/Modal/index.jsx
+++ frontend/src/pages/Application/components/Modal/index.jsx
@@ -1,81 +1,109 @@
 import { forwardRef, useEffect, useState } from "react";
 import Button from "../../../../components/Button";
 import MenuInfo from "../MenuInfo";
-import { ApplicationModalContainer, ButtonWrapper, ItemListConatiner, MenuInfoListContainer, MenuInfoListTitleWrapper, MenuInfoListWrapper, ModalBackground } from "./index.style";
+import { ApplicationModalContainer, ButtonWrapper, ItemListConatiner, ModalBackground, RequestContainer, RequestListContainer } from "./index.style";
 
+const dummyCafeList = [
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+	{ imgPath: "", name: "에이바우트" },
+];
+const dummyMenuList = [
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+	{ imgPath: "", name: "아메리카노" },
+];
+
+const dummyRequestList = ["연하게", "샷추가", "시럽추가", "짜게", "맛있게"];
 export default function ApplicationModal(props) {
 	const { menuList, setMenuList, refs, setIsModalOpen } = props;
 	const [modalIdx, setModalIdx] = useState(0);
 	const [menuInfo, setMenuInfo] = useState({ cafe: "", menu: "" });
+	const [requestList, setRequestList] = useState([]);
 	const [menuInfoList, setMenuInfoList] = useState(menuList);
-	const dummyCafeList = [
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-		{ imgPath: "", name: "에이바우트" },
-	];
-	const dummyMenuList = [
-		{ imgPath: "", name: "아메리카노" },
-		{ imgPath: "", name: "아메리카노" },
-		{ imgPath: "", name: "아메리카노" },
-		{ imgPath: "", name: "아메리카노" },
-		{ imgPath: "", name: "아메리카노" },
-		{ imgPath: "", name: "아메리카노" },
-		{ imgPath: "", name: "아메리카노" },
-	];
+	const [animationName, setAnimationName] = useState("slide-in");
+	const [isRequestOpen, setIsRequestOpen] = useState(false);
+
 	const handleItemClick = (name) => {
 		setMenuInfo((prev) => {
 			const newState = { ...prev };
 			newState[modalIdx === 0 ? "cafe" : "menu"] = name;
 			return newState;
 		});
+		if (modalIdx === 1) {
+			setIsRequestOpen(true);
+			return;
+		}
 		setModalIdx((prev) => prev + 1);
 	};
+	const removeRequestModal = () => {
+		if (!isRequestOpen) {
+			return;
+		}
+		setAnimationName("slide-out");
+		setTimeout(() => {
+			setIsRequestOpen(false);
+			setAnimationName("slide-in");
+		}, 450);
+	};
+	const handleRequestClick = (request) => {
+		if (requestList.includes(request)) {
+			setRequestList((prev) => prev.filter((req) => req !== request));
+			return;
+		}
+		setRequestList((prev) => [...prev, request]);
+	};
+	useEffect(() => {
+		setIsRequestOpen(false);
+		setRequestList([]);
+	}, [modalIdx]);
 	useEffect(() => {
 		if (modalIdx === 2) {
-			setMenuInfoList((prev) => [...prev, { ...menuInfo, num: 1, request: "" }]);
+			setMenuInfoList((prev) => [...prev, { ...menuInfo, num: 1, request: requestList.join(", ") }]);
 		}
 	}, [modalIdx]);
 
-	const handleMenuNum = (action, idx) => {
-		const newList = [...menuInfoList];
-		const newState = { ...newList[idx] };
-		if (action === "minus") {
-			if (newState.num === 1) {
-				setMenuInfoList((prev) => newList.filter((item, index) => index !== idx));
-				return;
-			}
-			newState.num -= 1;
-			newList[idx] = newState;
-			setMenuInfoList(newList);
-			return;
-		}
-		newState.num += 1;
-		newList[idx] = newState;
-		setMenuInfoList(newList);
-	};
-	const handleChangeRequest = (e, idx) => {
-		const newList = [...menuInfoList];
-		const newState = { ...newList[idx] };
-		newState.request = e.target.value;
-		newList[idx] = newState;
-		setMenuInfoList(newList);
-	};
 	return (
 		<ModalBackground>
 			<ApplicationModalContainer ref={refs}>
@@ -83,24 +111,51 @@ export default function ApplicationModal(props) {
 				{modalIdx === 2 ? (
 					<MenuInfo menuInfoList={menuInfoList} setMenuInfoList={setMenuInfoList} />
 				) : (
-					<ItemListConatiner>
-						{(modalIdx === 0 ? dummyCafeList : dummyMenuList).map((item) => (
-							<div onClick={() => handleItemClick(item.name)}>
+					<ItemListConatiner onScroll={removeRequestModal}>
+						{(modalIdx === 0 ? dummyCafeList : dummyMenuList).map((item, idx) => (
+							<div onClick={() => handleItemClick(item.name)} key={`${item.name} ${idx}`}>
 								<img src={item.imaPath} />
 								<span>{item.name}</span>
 							</div>
 						))}
 					</ItemListConatiner>
 				)}
+				{isRequestOpen && (
+					<RequestListContainer className={animationName}>
+						<button>aa</button>
+						<div>
+							{dummyRequestList.map((request, idx) => (
+								<RequestContainer
+									key={`${request} ${idx}`}
+									onClick={() => {
+										handleRequestClick(request);
+									}}
+									isSelected={requestList.includes(request)}
+								>
+									<span>{request}</span>
+								</RequestContainer>
+							))}
+						</div>
+					</RequestListContainer>
+				)}
 				<ButtonWrapper>
-					<button onClick={() => setModalIdx((prev) => (prev === 0 ? prev : prev - 1))}>뒤로가기</button>
-					<Button
-						handleClick={() => {
-							setMenuList(menuInfoList);
-							setIsModalOpen(false);
-						}}
-						content="등록하기"
-					></Button>
+					{modalIdx === 2 ? <button onClick={() => setModalIdx(0)}>메뉴 추가하기</button> : <button onClick={() => setModalIdx((prev) => (prev === 0 ? prev : prev - 1))}>뒤로가기</button>}
+					{isRequestOpen ? (
+						<Button
+							handleClick={() => {
+								setModalIdx((prev) => prev + 1);
+							}}
+							content="다음으로"
+						></Button>
+					) : (
+						<Button
+							handleClick={() => {
+								setMenuList(menuInfoList);
+								setIsModalOpen(false);
+							}}
+							content="등록하기"
+						></Button>
+					)}
 				</ButtonWrapper>
 			</ApplicationModalContainer>
 		</ModalBackground>
