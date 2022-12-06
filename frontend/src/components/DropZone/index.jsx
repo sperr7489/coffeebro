dc25a99d48478adff38fd0f68631b4272ae1823b
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-12-06 18:34:16 +0900

    feat/#5 feat : 이미지 업로드, 수정 추가

---
+++ frontend/src/components/DropZone/index.jsx
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+const DropZone = ({ setFile, setImage, children }) => {
+	const onDrop = useCallback((acceptedFiles) => {
+		acceptedFiles.forEach((file) => {
+			const reader = new FileReader();
+			setFile(file);
+			const bloburl = URL.createObjectURL(file);
+			setImage(bloburl);
+			reader.onabort = () => console.log("file reading was aborted");
+			reader.onerror = () => console.log("file reading has failed");
+			reader.onload = () => {
+				// Do whatever you want with the file contents
+				const binaryStr = reader.result;
+			};
+			reader.readAsArrayBuffer(file);
+		});
+	}, []);
+	const { getRootProps, getInputProps } = useDropzone({ onDrop });
+	return (
+		<div {...getRootProps()} id="dropzone">
+			<input {...getInputProps()} />
+			{children}
+		</div>
+	);
+};
+
+export default DropZone;
---
+++ frontend/src/pages/Mypage/components/index.style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InfoImageConatiner = styled.div`
+	width: 300px;
+	height: 300px;
+	border: 1px solid black;
+`;
+
+export const DropZoneContainer = styled.div`
+	width: 100%;
+	height: 300px;
+	background: url("../../../assets/images/img_add.svg");
+	cursor: pointer;
+	img {
+		width: 100%;
+		height: 300px;
+	}
+`;
+
+export const InformationContainer = styled.div`
+	margin-top: 33px;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 22px;
+	text-align: center;
+`;
