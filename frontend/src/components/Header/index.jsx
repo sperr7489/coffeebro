ddd941b217f62edc5f91beb34f65bc44221500f3
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-12-06 18:33:43 +0900

    feat/#5 feat : header 마이페이지 이동 추가

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,19 +1,21 @@
-import { HeaderContainer, RightChildContainer } from './index.style';
-import { icAlarm, icUser, icMenu } from '@/assets/icons';
+import { HeaderContainer, RightChildContainer } from "./index.style";
+import { icAlarm, icUser, icMenu } from "@/assets/icons";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <HeaderContainer>
-      <img src={icMenu} />
-      <span>커피가게아저씨</span>
-      <RightChildContainer>
-        <button>
-          <img src={icAlarm} />
-        </button>
-        <button>
-          <img src={icUser} />
-        </button>
-      </RightChildContainer>
-    </HeaderContainer>
-  );
+	const navigate = useNavigate();
+	return (
+		<HeaderContainer>
+			<img src={icMenu} />
+			<span>커피가게아저씨</span>
+			<RightChildContainer>
+				<button>
+					<img src={icAlarm} />
+				</button>
+				<button onClick={() => navigate("/mypage")}>
+					<img src={icUser} />
+				</button>
+			</RightChildContainer>
+		</HeaderContainer>
+	);
 }
