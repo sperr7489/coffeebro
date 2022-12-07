5d9ea26f711e0111bfce7387fd60e6d61418b8cd
20mini <dlqslek10@gmail.com>
2022-12-08 03:25:24 +0900

    refactor: user 테이블에 nickname 추가 feat: nickname check 기능 구현 fix: userController의 login에서 email을 const 에서 let으로 변경 fix: cafeDao의 getOptionList에서 coffeebro. 삭제

--- backend/Router/User/userDao.js
+++ backend/Router/User/userDao.js
@@ -12,8 +12,8 @@ exports.emailCheck = async (connection, email) => {
 // DB에 유저 등록
 exports.insertUser = async (connection, insertUserParams) => {
   const insertUserQuery = `
-       INSERT INTO user (email, passwd, userName, department, sex, studentId)
-          VALUES (?,?,?,?,?,?);    
+       INSERT INTO user (email, passwd, userName, department, sex, studentId, nickname)
+          VALUES (?,?,?,?,?,?,?);    
       `;
   const [insertUserRow] = await connection.query(
     insertUserQuery,
@@ -50,10 +50,10 @@ exports.CheckPasswd = async (connection, email) => {
   return signInCheckPasswdRow;
 };
 
-// 유저의 인덱스와 이름 가져오기.
+// 유저의 인덱스와 이름과 닉네임 가져오기.
 exports.getUserShortInfo = async (connection, email) => {
   const getUserShortInfoQuery = `
-    select userIdx, userName from user 
+    select userIdx, userName, nickname from user 
     where email = ?;
   `;
   const [[getUserShortInfoRow]] = await connection.query(
@@ -268,7 +268,7 @@ exports.existsDeliverApply = async (
 // 배달 대행을 하겠다고 신청한 내역 가져오기
 exports.getApplyInfos = async (connection, userIdx) => {
   const getAgentIdxsQuery = `
-  select sa.serviceApplicationIdx,da.deliveryAgentIdx,u.userName,u.department,u.studentId,u.sex,u.userImg,ifnull(u.deliveryAgentScore,0) as "배달 대행 평점", sa.receiptTime, c.cafeName, d.drinkName,ifnull(do.optionName,"옵션X"),(d.price + ifnull(do.price,0)) as price   from deliveryApplication da 
+  select sa.serviceApplicationIdx,da.deliveryAgentIdx,u.userName,u.nickname,u.department,u.studentId,u.sex,u.userImg,ifnull(u.deliveryAgentScore,0) as "배달 대행 평점", sa.receiptTime, c.cafeName, d.drinkName,ifnull(do.optionName,"옵션X"),(d.price + ifnull(do.price,0)) as price   from deliveryApplication da 
   left join serviceApplication sa on da.serviceApplicationIdx = sa.serviceApplicationIdx
   left join RequestDrinkList rdl on rdl.serviceApplicationIdx = sa.serviceApplicationIdx
   left join  drink d on d.drinkIdx = rdl.drinkIdx
@@ -313,10 +313,10 @@ exports.updateStatusOnAccept = async (
 };
 
 // for 마이페이지
-// user의 이름, 신청자 평점, 대행자 평점, 이미지 가져오기
+// 유저의 이름, 닉네임, 신청자 평점, 대행자 평점, 사진 가져오기
 exports.getUserInfo = async (connection, userIdx) => {
   const getUserInfoQuery = `
-    select userName, applicantScore, deliveryAgentScore, userImg from user 
+    select userName, nickname, applicantScore, deliveryAgentScore, userImg from user 
     where userIdx = ?;
   `;
   const [[getUserInfoRow]] = await connection.query(
@@ -341,4 +341,15 @@ exports.getMostVisitedCafeIdx = async (connection, userIdx) => {
       userIdx
   );
   return getMostVisitedCafeIdxRow;
+};
+
+// 닉네임 존재 여부
+exports.nicknameCheck = async (connection, nickname) => {
+  const nicknameCheckQuery = `
+      select exists (
+      select * from user where nickname = ?
+      ) as exist
+  `;
+  const [[nicknameCheckRow]] = await connection.query(nicknameCheckQuery, nickname);
+  return nicknameCheckRow.exist;
 };
