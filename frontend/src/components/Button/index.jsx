c7b52fc15376d5f177d2f12df114963ae7eaa7e7
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-12-07 22:15:46 +0900

    feat/#5 style : 코드 스타일 수정 및 css 추가

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -1,6 +1,6 @@
-import { ButtonContainer } from './index.style';
+import { ButtonContainer } from "./index.style";
 
 export default function Button(props) {
-  const { content, handleClick } = props;
-  return <ButtonContainer onClick={handleClick}>{content}</ButtonContainer>;
+	const { content, handleClick } = props;
+	return <ButtonContainer onClick={handleClick}>{content}</ButtonContainer>;
 }
--- frontend/src/components/Button/index.style.js
+++ frontend/src/components/Button/index.style.js
@@ -1,10 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-  width: 200px;
-  height: 80px;
-  font-size: 28px;
-  background-color: #2eb233;
-  border-radius: 10px;
-  border: none;
+	width: 200px;
+	height: 80px;
+	font-size: 28px;
+	background-color: #2eb233;
+	border-radius: 10px;
+	border: none;
+	cursor: pointer;
 `;
