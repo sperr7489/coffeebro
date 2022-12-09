a4776539b4739df43cd6ddf128d8299912329065
JAEYEONsss <y718j@ajou.ac.kr>
2022-12-10 01:01:55 +0900

    access token 쿠키 저장, 배달 신청 api 연결

--- frontend/src/pages/Mypage/index.jsx
+++ frontend/src/pages/Mypage/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../../components/Button';
+import LoginCheck from '../Login/components/LoginCheck';
 import InfoImage from './components/InfoImage';
 import { MypageContainer, UserInfoContainer } from './index.style';
 
@@ -12,6 +13,7 @@ export default function MyPage() {
   };
   return (
     <MypageContainer>
+      <LoginCheck />
       <InfoImage image={image} setImage={setImage} file={file} setFile={setFile}></InfoImage>
       <UserInfoContainer>
         <div>
