4d7e93b3f04d93e186b89b853519365ff211ef59
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-12-12 05:24:34 +0900

    feat/#6 feat : 채팅페이지 소켓 연결 및 테스트

--- frontend/src/pages/Chat/index.jsx
+++ frontend/src/pages/Chat/index.jsx
@@ -1,16 +1,37 @@
-import Header from '../../components/Header';
+import { useEffect, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { authApi } from '../../../axios.config';
 import LoginCheck from '../Login/components/LoginCheck';
 import Chatting from './components/Chatting';
 import UserList from './components/UserList';
 import { ChatPageContainer, ChatMainContainer } from './index.style';
 
 export default function ChatPage() {
+  const [chatRoomInfo, setChatRoomInfo] = useState([]);
+  const [chatRoomIdx, setChatRoomIdx] = useState(0);
+  const params = useParams();
+
+  useEffect(() => {
+    async function getChatData() {
+      authApi.get(`/chat/rooms`).then((res) => {
+        setChatRoomInfo(res.data.result);
+        if (params.chatRoomIdx) {
+          setChatRoomIdx(Number(params.chatRoomIdx));
+        }
+      });
+    }
+    getChatData();
+  }, []);
   return (
     <ChatPageContainer>
+      <LoginCheck />
       <ChatMainContainer>
-        <LoginCheck />
-        <Chatting />
-        <UserList />
+        <Chatting chatRoomIdx={chatRoomIdx} chatRoomInfo={chatRoomInfo} />
+        <UserList
+          chatRoomInfo={chatRoomInfo}
+          setChatRoomIdx={setChatRoomIdx}
+          chatRoomIdx={chatRoomIdx}
+        />
       </ChatMainContainer>
     </ChatPageContainer>
   );
